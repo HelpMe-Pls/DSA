d21c2c2c1eb47377f766a27d7c58826beb4df30c
Lê Tấn Khôi <43715386+HelpMe-Pls@users.noreply.github.com>
2023-05-12 11:32:15 +0700

    Graph

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -1,3 +1,40 @@
 export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+  const traversedPath = new Array(graph.length).fill(false)
 
+  // if it's != -1 then there's a connection with the current node
+  // e.g. prev[69] means the connection with the current node with the weight of 69
+  const prev = new Array(graph.length).fill(-1)
+
+  traversedPath[source] = true    // start from the source node
+  const q: number[] = [source]    // `q` is the path we're forming, starting with a `source` node
+
+  do {
+    const curr = q.shift() as number
+    const adjs = graph[curr]
+
+    if (curr == needle) break  // found it
+
+    for (let i = 0; i < adjs.length; ++i) {
+      // `adjs[i] == 0` means there is no connection 
+      if (adjs[i] == 0 || traversedPath[i]) continue
+
+      traversedPath[i] = true
+      prev[i] = curr
+      q.push(i)
+    }
+  } while (q.length)
+
+  if (prev[needle] == -1) return null // unable to form a path to the target node
+
+  // build output path by reversing the `prev` array
+  let curr = needle
+  const path: number[] = []
+
+  while (prev[curr] != -1) {
+    path.push(curr)
+    curr = prev[curr]
+    console.log(path)
+  }
+
+  return [source].concat([...path].reverse())
 }
--- src/day1/DFSGraphList.ts
+++ src/day1/DFSGraphList.ts
@@ -1,3 +1,40 @@
-export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+const dir = [
+  [-1, 0],  // left
+  [1, 0],   // right
+  [0, -1],  // down
+  [0, 1],   // up
+]
 
+function steps(graph: WeightedAdjacencyList, curr: number, needle: number, seen: boolean[], path: number[]): boolean {
+  // Hit previously visited point:
+  if (seen[curr]) return false
+
+  // =========== Recursive calls (creating a path) ===============
+  // Pre-recursion:
+  seen[curr] = true
+  path.push(curr)
+  if (curr == needle) return true
+
+
+  // Recursive calls:
+  const list = graph[curr]
+  for (const i of list) {
+    const edge = i
+    if (steps(graph, edge.to, needle, seen, path)) return true
+  }
+
+  // Post-recursion:
+  path.pop()
+
+  return false
+}
+
+export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+  const traversedPath = new Array(graph.length).fill(false)
+  const currentPath: number[] = []
+
+  steps(graph, source, needle, traversedPath, currentPath)
+
+  if (currentPath.length == 0) return null
+  return currentPath
 }
